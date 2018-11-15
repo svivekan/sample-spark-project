db554034bbc71b8c0be4d28b637b9657943cf431
svivekan <sanjayvivek@hotmail.com>
2018-11-15 11:41:04 +0000

    gitignore added

--- src/test/scala/com/github/mrpowers/spark/pika/chaining/ChainingSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/pika/chaining/ChainingSpec.scala
@@ -13,14 +13,14 @@ class ChainingSpec
   test("withGreeting and withFarewell") {
     import spark.implicits._
 
-    val df = Seq("dog", "cat").toDF("subject")
+    val df = Seq("man", "woman").toDF("subject")
 
     val actualDF = df
       .transform(Chaining.withGreeting)
       .transform(Chaining.withFarewell)
     val actualDS = actualDF.as[Greeting]
 
-    val expectedDS = Seq(Greeting("dog", "hello", "goodbye"), Greeting("cat", "hello", "goodbye")).toDS()
+    val expectedDS = Seq(Greeting("man", "hello", "goodbye"), Greeting("woman", "hello", "goodbye")).toDS()
 
     actualDS.except(expectedDS).count() should be(0)
   }
