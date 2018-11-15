e5d17f78990af50a2607365c28a98693140daf09
svivekan <sanjayvivek@hotmail.com>
2018-11-15 11:49:57 +0000

    Logging removed in sparksession builder

--- src/main/scala/com/github/mrpowers/spark/pika/core/SparkSessionWrapper.scala
+++ src/main/scala/com/github/mrpowers/spark/pika/core/SparkSessionWrapper.scala
@@ -1,12 +1,10 @@
 package com.github.mrpowers.spark.pika.core
 
-import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql.SparkSession
 
 trait SparkSessionWrapper {
 
   lazy val spark: SparkSession = {
-    LogManager.getRootLogger.setLevel(Level.ERROR)
     SparkSession
       .builder()
       .master("local")
